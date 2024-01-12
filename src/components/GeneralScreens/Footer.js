c925acdde446426ecd631d71660a887884f06e2c
Shubh942 <93862397+Shubh942@users.noreply.github.com>
2024-01-13 00:10:53 +0530

    first commit

---
+++ src/components/GeneralScreens/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../../Css/Footer.css";
+
+const Footer = () => {
+  return (
+    <div>
+      <div className="footer"></div>
+      <div className="copyright">
+        <p className="copyright-blog">
+          © 2024 Shubh Mehta. All Rights Reserved
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
